160d6a2ddb90b0acc33e837ede7648ee30bd9a09
WillMR94 <Willian.M.Rosa@gmail.com>
2023-06-18 20:20:45 -0300

    ''

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
+})
+
+/*
+build: {
     rollupOptions: {
       external: [
         "/swiper",
         "/Swiper/react"
       ]}}
-})
+**/
